ebd76b7f5c407dc77f2b6f1752582405bacf4d29
moxy <louisemoxy@gmail.com>
2021-01-17 15:12:12 +0000

    Add padding option on col component

--- components/col-wysiwyg/index.tsx
+++ components/col-wysiwyg/index.tsx
@@ -8,9 +8,9 @@ export const ColWysiwyg = ({ index, data }) => {
     return (
         <BlocksControls index={index} insetControls>
             <Box align="center">
-                <Box width="xlarge" pad="medium" direction="row" wrap>
+                <Box width="xlarge" pad={data.padding ? 'medium' : {top: 'none', bottom: 'none', right: 'medium', left: 'medium'}} direction="row" wrap>
                     {cols.map((index) => (
-                        <Box pad="small" key={index} basis={+data.cols === 2 ? "1/2" : "1/3"} className="content" width={{min: '300px'}} flex justify={data[`justify${index+1}`]}>
+                        <Box pad="small" key={index} basis={+data.cols === 2 ? "1/2" : "1/3"} className="content" width={{ min: '300px' }} flex justify={data[`justify${index + 1}`]} style={{ textAlign: data[`textAlign${index + 1}`] || 'left' }}>
                             <InlineWysiwyg
                                 name={`html${index + 1}`}
                                 format="html"
@@ -44,7 +44,11 @@ export const ColWysiwyg_template = {
         html3: "Add Content here",
         justify1: "center",
         justify2: "center",
-        justify3: "center"
+        justify3: "center",
+        textAlign1: "left",
+        textAlign2: "left",
+        textAlign3: "left",
+        padding: true,
     },
     fields: [
         {
@@ -55,10 +59,22 @@ export const ColWysiwyg_template = {
             default: 3
         },
         {
+            name: "padding",
+            label: "Top and bottom padding",
+            component: "toggle",
+            default: true
+        },
+        {
             name: "justify1",
             label: "Justify Content column 1",
             component: "select",
             options: ['start', 'center', 'end', 'between', 'around'],
+        },
+        {
+            name: "textAlign1",
+            label: "Text align column 1",
+            component: "select",
+            options: ['left', 'center', 'right']
         },
         {
             name: "html1",
@@ -72,6 +88,12 @@ export const ColWysiwyg_template = {
             options: ['start', 'center', 'end', 'between', 'around'],
         },
         {
+            name: "textAlign2",
+            label: "Text align column 1",
+            component: "select",
+            options: ['left', 'center', 'right']
+        },
+        {
             name: "html2",
             label: "Content column 2",
             component: "html",
@@ -81,6 +103,12 @@ export const ColWysiwyg_template = {
             label: "Justify Content column 3",
             component: "select",
             options: ['start', 'center', 'end', 'between', 'around'],
+        },
+        {
+            name: "textAlign3",
+            label: "Text align column 1",
+            component: "select",
+            options: ['left', 'center', 'right']
         },
         {
             name: "html3",
